83a7575aacdf9aabf543dbf07df25cf98424b44c
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-11 23:31:12 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 {
    int i;
-    for (i = 0;i < 10;i++)
+    for (i = 0; i < 10; i++)
         putchar(48 + i);    
     return 0;
 }
