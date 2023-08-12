64c3c921cb5ae1ce1570a2d41e76d4a74e7d6458
= <=>
2023-08-12 16:16:12 +0300

    Hello

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+ /** 
+  * main - entry point 
+  * Return: 1 (error) 
+  */ 
+ int main(void) 
+ { 
+ char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"; 
+ fprintf(stderr, "%s\n", message); 
+ return (1); 
+ }
