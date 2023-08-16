521f2eeb79325e483b50d14fbd5f2ab617e828ea
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-16 22:32:53 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,16 +2,20 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _isalpha - writes the character c to stdout
+ * @n: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _isalpha(int c)
 {
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'))
-		return (1);
-	else
-		return (0);
+    if ((c >= 65 && c <= 90)  || (c >= 97 && c <=122))
+    {
+        return (1);
+    }
+    else
+    {
+        return(0);
+    }
 }
