81f257a0c2489e903d91a32ec090d8688801b220
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-16 20:50:52 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,7 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_sign - writes the character c to stdout
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -20,6 +19,8 @@ int print_sign(int n)
 	}
 	else 
 	{
-		putchar('-');
+		_putchar('-');
 	}
+	
+	return (0);
 }
