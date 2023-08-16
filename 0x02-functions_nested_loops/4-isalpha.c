d2d677e3cf42a43bc108ae24f458d7de46979e89
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-16 22:34:56 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 
 /**
  * _isalpha - writes the character c to stdout
- * @n: The character to print
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
