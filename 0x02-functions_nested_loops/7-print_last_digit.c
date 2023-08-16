323f1bb19371eef0cbf60c32658ac6c56db524c4
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-16 21:35:57 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @n: The number to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -11,7 +11,8 @@
 int print_last_digit(int n)
 {
        int remainder;
+
        remainder = n % 10;
        _putchar(48 + remainder);
-       return (0);
+       return (remainder);
 }
