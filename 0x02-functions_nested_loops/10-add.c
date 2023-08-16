03061aeefae1408c03fe747beac5ec0675876c88
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-16 22:48:34 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
+ * add - writes the int x and y to stdout
+ * @x: The intiger to print
+ *@y: The intiger to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  *
