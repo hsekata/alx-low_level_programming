be78d2983b65edb0f9ddfc39b3a5dfeb901b9af2
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-16 22:21:30 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,20 +10,8 @@
  */
 int _isalpha(int c)
 {
-    if (c >= 65 && c <= 90)
-    {
-        _putchar('1');
-        return (1);
-    }
-    else if (c >= 97 && c <=122)
-    {
-        _putchar('1');
-        return (1);
-    }
-    else
-    {
-        _putchar('0');
-        return (0);
-    }
-    
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
