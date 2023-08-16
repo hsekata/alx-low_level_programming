afe61a8efdf8de8460639d302e4ce01e826eb60a
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-16 19:25:57 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char start;
-    
-    	for(start = 'a'; start <= 'z'; start++)
+	for(start = 'a'; start <= 'z'; start++)
     	{
         	_putchar(start);
-        
-    	}
+        }
 	_putchar('\n');
 }
