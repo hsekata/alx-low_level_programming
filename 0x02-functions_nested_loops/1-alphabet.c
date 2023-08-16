bc12c1c3d02eb1d78290a6a7a1ffa7964d94ee2c
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-16 19:29:21 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,10 @@
 void print_alphabet(void)
 {
 	char start;
-	for(start = 'a'; start <= 'z'; start++)
-    	{
-        	_putchar(start);
+	
+	for (start = 'a'; start <= 'z'; start++)
+	{
+		_putchar(start);
         }
 	_putchar('\n');
 }
