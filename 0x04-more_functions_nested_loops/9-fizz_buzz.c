662cd18241d4c971f51ea035fa2aaffa8eef9e37
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-17 20:04:34 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,23 @@ int main(void)
 
     	for (i = 1; i <= 100; i++)
     	{
-       	if (((i % 3) != 0) && ((i % 5) != 0))
-       	{
-            	printf("%d ", i);
-       	}
-       	else if (((i % 15) == 0) && (i % 3) == 0)
-        {
-            	printf("FizzBuzz ");
+            if (((i % 3) != 0) && ((i % 5) != 0))
+           	{
+                	printf("%d ", i);
+           	}
+           	else if (((i % 15) == 0) && (i % 3) == 0)
+            {
+                	printf("FizzBuzz ");
+            }
+           	else if ((i % 3) == 0)
+            {
+                	printf("Fizz ");
+            }
+            else if ((i % 5) == 0)
+            {
+                	printf("Buzz ");
+            }
         }
-       	else if ((i % 3) == 0)
-        {
-            	printf("Fizz ");
-        }
-        else if ((i % 5) == 0)
-        {
-            	printf("Buzz ");
-        }
-        
-    }
     printf("\n");
     return (0);
 }
