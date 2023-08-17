d997d818082adb00d19406176276aa9213be7f66
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-17 22:30:52 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,8 +2,6 @@
 
 /**
  * add - writes the int x and y to stdout
- * @x: The intiger to print
- *@y: The intiger to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  *
