e07d752a6d598d1639a789dceb3713a8a01d36aa
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-17 22:43:23 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,21 +1,26 @@
 #include "main.h"
 
 /**
- * print_square - function that prints a square,
- * followed by a new line.
+ * print_triangle -  a function that prints a triangle, followed by a new line.
  * @size: An input integer
  * Return: Always 0
  */
-void print_square(int size)
+void print_triangle(int size)
 {
-	int i, j;
+	int i = 0, j, n = size - 1;
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
-				_putchar('#');
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
 			_putchar('\n');
 		}
 	}
