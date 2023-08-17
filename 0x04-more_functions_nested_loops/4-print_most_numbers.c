2f8f9dce577ac86fa169394c6fe58b4261dcbbe8
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-17 22:29:15 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,13 +3,6 @@
 /**
  * print_most_numbers - Test the _isalpha function
  *
- * @n: Number to pass to _isalpha function
- */
-
-
-/**
- * main - check the code for Holberton School students.
- *
  * Return: Always 0.
  */
 void print_most_numbers(void)
