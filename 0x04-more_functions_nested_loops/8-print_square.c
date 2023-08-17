fdf6a2b0ac911dede551f6f02234d07d2959117f
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-17 20:39:41 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
 /**
- * print_square - function that prints hash.
- *
- * @size: Number  function
+ * print_square - Write a function that prints a square,
+ * followed by a new line.
+ * @size: An input integer
+ * Return: Always 0
  */
 void print_square(int size)
-{   
-    	int i, j;
+{
+	int i, j;
 
-    	if (size > 0)
+	if (size > 0)
 	{
-        	for (i = 0; i < size; i++)
-        	{
-            		for (j = 0; j < size ; j++)
-            		{
-                		_putchar('#');
-            		}
-  		_putchar('\n');
-        	}
-    	}
-    	else
-    	{
-        	_putchar('\n');
-    	}
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
