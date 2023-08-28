892d6dbac7364b612f51de9d6c585026bcf4f7fc
hsekata <122925536+hsekata@users.noreply.github.com>
2023-08-28 21:55:42 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,13 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
-  unsigned int i;
+    unsigned int i;
 
-  i = 0;
-  while (i < n)  /*Delacring WHILE*/
-  {
-    *(dest + i) = *(src + i);
-    i++;
-  }
-  return (dest);
+    i = 0;
+    while (i < n)  /*Delacring WHILE*/
+    {
+        *(dest + i) = *(src + i);
+        i++;
+    }
+    return (dest);
 }
