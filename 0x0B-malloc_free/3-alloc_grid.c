98493bf54050b654094d556786dad59b21619c72
hsekata <122925536+hsekata@users.noreply.github.com>
2023-09-05 22:03:59 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,6 +9,10 @@ int **alloc_grid(int width, int height)
 {
 	int **array, i, j;
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 	array = (int **) malloc(width * sizeof(int *));
 	if (array == NULL)
 		return (NULL);
